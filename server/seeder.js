2e29d25e97c3ca790cf6fc5fa9788197b87ddf42
Sathurshanan Kandhasamy <sath7samy@gmail.com>
2023-12-02 15:14:46 +1000

    Add sample data seeder file.

---
+++ server/seeder.js
@@ -0,0 +1,59 @@
+import mongoose from 'mongoose';
+import 'dotenv/config';
+import USERS from './data/users.js';
+import PRODUCTS from './data/products.js';
+import USER from './models/user.js';
+import PRODUCT from './models/product.js';
+import ORDER from './models/order.js';
+import CONNECT_TO_DATABASE from './config/database.js';
+
+// Connects to MongoDB database.
+CONNECT_TO_DATABASE();
+
+// Inserts data in database.
+const IMPORT_DATA = async () => {
+  try {
+    await ORDER.deleteMany();
+    await PRODUCT.deleteMany();
+    await USER.deleteMany();
+
+    const CREATED_USERS = await USER.insertMany(USERS);
+    const ADMIN_USER = CREATED_USERS[0]._id;
+
+    const SAMPLE_PRODUCTS = PRODUCTS.map((product) => {
+      return {
+        ...product,
+        user: ADMIN_USER,
+      };
+    });
+    await PRODUCT.insertMany(SAMPLE_PRODUCTS);
+
+    console.log('Data imported.');
+    process.exit();
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+// Deletes data in database.
+const DESTROY_DATA = async () => {
+  try {
+    await ORDER.deleteMany();
+    await PRODUCT.deleteMany();
+    await USER.deleteMany();
+
+    console.log('Data destroyed.');
+    process.exit();
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+// If argument is -d delete data else insert data in database.
+if (process.argv[2] === '-d') {
+  DESTROY_DATA();
+} else {
+  IMPORT_DATA();
+}
