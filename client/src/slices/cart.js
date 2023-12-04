cd838b8750a2d8bea8c5016efe467c415e5c0dc5
Sathurshanan Kandhasamy <sath7samy@gmail.com>
2023-12-04 21:35:38 +1000

    Add cart utilities file.

--- client/src/slices/cart.js
+++ client/src/slices/cart.js
@@ -1,12 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { UPDATE_CART } from '../utilities/cart';
 
 const INITIAL_STATE = localStorage.getItem('cart')
   ? JSON.parse(localStorage.getItem('cart'))
   : { cartItems: [] };
-
-const ADD_DECIMALS = (number) => {
-  return Math.round((number * 100) / 100).toFixed(2);
-};
 
 const CART = createSlice({
   name: 'cart',
@@ -23,29 +20,10 @@ const CART = createSlice({
         state.cartItems = [...state.cartItems, ITEM];
       }
 
-      // Calculates cart items price.
-      state.itemsPrice = ADD_DECIMALS(
-        state.cartItems.reduce(
-          (accumulator, item) => accumulator + item.price * item.qty,
-          0
-        )
-      );
-      // Calculates shipping price. If order is over $100 then free, else $10 shipping.
-      state.shippingPrice = ADD_DECIMALS(state.itemsPrice > 100 ? 0 : 10);
-      // Calculates tax price, tax is 15%.
-      state.taxPrice = ADD_DECIMALS(Number(0.15 * state.itemsPrice).toFixed(2));
-      // Calculates total price.
-      state.totalPrice = (
-        Number(state.itemsPrice) +
-        Number(state.shippingPrice) +
-        Number(state.taxPrice)
-      ).toFixed(2);
-      // Saves the cart in local storage.
-      localStorage.setItem('cart', JSON.stringify(state));
+      return UPDATE_CART(state);
     },
   },
 });
 
 export const { addToCart: ADD_TO_CART } = CART.actions;
-
 export default CART.reducer;
---
+++ client/src/utilities/cart.js
@@ -0,0 +1,26 @@
+export const ADD_DECIMALS = (number) => {
+  return Math.round((number * 100) / 100).toFixed(2);
+};
+
+export const UPDATE_CART = (state) => {
+  // Calculates cart items price.
+  state.itemsPrice = ADD_DECIMALS(
+    state.cartItems.reduce(
+      (accumulator, item) => accumulator + item.price * item.qty,
+      0
+    )
+  );
+  // Calculates shipping price. If order is over $100 then free, else $10 shipping.
+  state.shippingPrice = ADD_DECIMALS(state.itemsPrice > 100 ? 0 : 10);
+  // Calculates tax price, tax is 15%.
+  state.taxPrice = ADD_DECIMALS(Number(0.15 * state.itemsPrice).toFixed(2));
+  // Calculates total price.
+  state.totalPrice = (
+    Number(state.itemsPrice) +
+    Number(state.shippingPrice) +
+    Number(state.taxPrice)
+  ).toFixed(2);
+  // Saves the cart in local storage.
+  localStorage.setItem('cart', JSON.stringify(state));
+  return state;
+};
