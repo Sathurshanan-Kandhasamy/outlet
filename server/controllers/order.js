f3ed6363707d76690d456e52aec093e69bf569c8
Sathurshanan Kandhasamy <sath7samy@gmail.com>
2023-12-14 10:46:42 +1000

    Implement create and get orders.

--- server/controllers/order.js
+++ server/controllers/order.js
@@ -5,21 +5,60 @@ import ORDER from '../models/order.js';
 // Route:        POST /api/orders
 // Access:       Private
 export const ADD_ORDER_ITEMS = ASYNC_HANDLER(async (request, response) => {
-  response.send('Add order items.');
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = request.body;
+  if (orderItems && orderItems.length === 0) {
+    response.status(400);
+    throw new Error('No order items.');
+  } else {
+    const order = new ORDER({
+      orderItems: orderItems.map((order) => ({
+        ...order,
+        product: order._id,
+        _id: undefined,
+      })),
+      user: request.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+    const createdOrder = await order.save();
+    response.status(201).json(createdOrder);
+  }
 });
 
 // Description:  Get logged in user orders.
 // Route:        GET /api/orders/myorders
 // Access:       Private
 export const GET_MY_ORDERS = ASYNC_HANDLER(async (request, response) => {
-  response.send('Get my orders.');
+  const orders = await ORDER.find({ user: request.user._id });
+  response.status(200).json(orders);
 });
 
 // Description:  Get order by id.
 // Route:        GET /api/orders/:id
 // Access:       Private
 export const GET_ORDER_BY_ID = ASYNC_HANDLER(async (request, response) => {
-  response.send('Get order by id.');
+  const order = await ORDER.findById(request.params.id).populate(
+    'user',
+    'name email'
+  );
+  if (order) {
+    response.status(200).json(order);
+  } else {
+    response.status(404);
+    throw new Error('Order not found.');
+  }
 });
 
 // Description:  Update order to paid.
