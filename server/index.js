621e51f609ecfa6a2a9e0727c084017609176c72
Sathurshanan Kandhasamy <sath7samy@gmail.com>
2023-12-24 15:13:23 +1000

    Setup multer and image upload endpoint.

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import 'dotenv/config';
 import cookieParser from 'cookie-parser';
@@ -6,6 +7,7 @@ import { notFound, errorHandler } from './middleware/error.js';
 import productRoutes from './routes/product.js';
 import userRoutes from './routes/user.js';
 import orderRoutes from './routes/order.js';
+import uploadRoutes from './routes/upload.js';
 const port = process.env.PORT;
 
 // Connects to MongoDB database.
@@ -27,10 +29,14 @@ app.get('/', (request, response) => {
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
 
 app.get('/api/config/paypal', (request, response) =>
   response.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
+
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.use(notFound);
 app.use(errorHandler);
