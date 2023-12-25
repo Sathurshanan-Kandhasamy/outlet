4c61d8d75e019e058d24c484257b65b18b95a396
Sathurshanan Kandhasamy <sath7samy@gmail.com>
2023-12-26 00:10:10 +1000

    Fix PayPal and price calculate bugs.

--- server/controllers/order.js
+++ server/controllers/order.js
@@ -1,29 +1,40 @@
 import asyncHandler from '../middleware/asyncHandler.js';
 import Order from '../models/order.js';
+import Product from '../models/product.js';
+import { calculatePrices } from '../utilities/calculatePrices.js';
+import {
+  verifyPayPalPayment,
+  checkIfNewTransaction,
+} from '../utilities/paypal.js';
 
 // Description:  Create a new order.
 // Route:        POST /api/orders
 // Access:       Private
 export const addOrderItems = asyncHandler(async (request, response) => {
-  const {
-    orderItems,
-    shippingAddress,
-    paymentMethod,
-    itemsPrice,
-    taxPrice,
-    shippingPrice,
-    totalPrice,
-  } = request.body;
+  const { orderItems, shippingAddress, paymentMethod } = request.body;
   if (orderItems && orderItems.length === 0) {
     response.status(400);
     throw new Error('No order items.');
   } else {
-    const order = new Order({
-      orderItems: orderItems.map((order) => ({
-        ...order,
-        product: order._id,
+    const itemsFromDatabase = await Product.find({
+      _id: { $in: orderItems.map((item) => item._id) },
+    });
+    const databaseOrderItems = orderItems.map((itemFromClient) => {
+      const matchingItemFromDatabase = itemsFromDatabase.find(
+        (itemsFromDatabase) =>
+          itemsFromDatabase._id.toString() === itemFromClient._id
+      );
+      return {
+        ...itemFromClient,
+        product: itemFromClient._id,
+        price: matchingItemFromDatabase.price,
         _id: undefined,
-      })),
+      };
+    });
+    const { itemsPrice, taxPrice, shippingPrice, totalPrice } =
+      calculatePrices(databaseOrderItems);
+    const order = new Order({
+      orderItems: databaseOrderItems,
       user: request.user._id,
       shippingAddress,
       paymentMethod,
@@ -65,8 +76,16 @@ export const getOrderById = asyncHandler(async (request, response) => {
 // Route:        PUT /api/orders/:id/pay
 // Access:       Private
 export const updateOrderToPaid = asyncHandler(async (request, response) => {
+  const { verified, value } = await verifyPayPalPayment(request.body.id);
+  if (!verified) throw new Error('Payment not verified.');
+
+  const isNewTransaction = await checkIfNewTransaction(Order, request.body.id);
+  if (!isNewTransaction) throw new Error('Transaction has been used before.');
+
   const order = await Order.findById(request.params.id);
   if (order) {
+    const paidCorrectAmount = order.totalPrice.toString() === value;
+    if (!paidCorrectAmount) throw new Error('Incorrect amount paid.');
     order.isPaid = true;
     order.paidAt = Date.now();
     order.paymentResult = {
@@ -76,7 +95,7 @@ export const updateOrderToPaid = asyncHandler(async (request, response) => {
       email_address: request.body.payer.email_address,
     };
     const updatedOrder = await order.save();
-    response.status(200).json(updatedOrder);
+    response.json(updatedOrder);
   } else {
     response.status(404);
     throw new Error('Order not found.');
